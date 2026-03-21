60cbeaa04e66cf1d0143d49fda3c6254b7f0c6ed
docuong0912 <ndcuong912@gmail.com>
2026-03-22 01:16:51 +0700

    add test component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TextfieldComponent } from './components/textfield/textfield.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TextfieldComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
