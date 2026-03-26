7cd6eab47477a8e5e7ee8cc88ef14640108e4d97
docuong0912 <ndcuong912@gmail.com>
2026-03-27 02:24:11 +0700

    handle timer and other special key

--- src/app/components/textfield/textfield.component.ts
+++ src/app/components/textfield/textfield.component.ts
@@ -1,4 +1,8 @@
-import { AfterViewInit, Component, ElementRef, Host, HostListener, input, signal, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, Host, HostListener, inject, Input, input, signal, ViewChild } from "@angular/core";
+import { CoreService } from "../../services/core.service";
+import { TimerService } from "../../services/timer.service";
+import { core } from "@angular/compiler";
+import { timer } from "rxjs";
 
 @Component({
     selector: "app-textfield",
@@ -7,33 +11,61 @@ import { AfterViewInit, Component, ElementRef, Host, HostListener, input, signal
 })
 export class TextfieldComponent {
     step = 0;
-    text = `The quick brown "fox" 'jumps' over the lazy dog. `;
+    wordCount = 0;
+    startIndex = 0;
+    errorCount = 0;
+    selectedPassage = input<string>('');
     checked: boolean[] = [];
-
+    private coreService = inject(CoreService);
+    private timerService = inject(TimerService);
     isActive(index: number): boolean {
         return this.currentIndex() === index;
     }
     currentIndex(): number {
         // Ensure the index is within bounds of the text length
-        return Math.min(this.step, Math.max(this.text.length - 1, 0));
+        return Math.min(this.step, Math.max(this.selectedPassage().length, 0));
     }
     validateCharacter(value: string): void {
-        if (this.text[this.currentIndex()] === value) {
+        if (this.selectedPassage()[this.currentIndex()] === value) {
             this.checked[this.currentIndex()] = true;
         }
         else {
             this.checked[this.currentIndex()] = false;
+            this.errorCount++;
         }
 
     }
     @HostListener("window:keydown", ["$event"])
     setCursor(event: KeyboardEvent): void {
-        this.validateCharacter(event.key);
+        if (this.step === 0) {
+            this.timerService.startTimer();
+        }
+        if (this.step >= this.selectedPassage().length - 1) {
+            this.timerService.stopTimer();
+            return;
+        }
         if (event.shiftKey) {
             if (event.key === "Shift") {
                 return;
             }
         }
+        if (event.key === "Backspace") {
+            if (this.step > 0) {
+                this.step--;
+            }
+            return;
+        }
+        if (event.key === "Spacebar" || event.key === " " && this.selectedPassage()[this.currentIndex()] === " ") {
+            let wordCheck = this.checked.slice(this.startIndex, this.currentIndex());
+            if (wordCheck.every((v) => v === true)) {
+                this.wordCount++;
+            }
+            this.startIndex = this.currentIndex() + 1;
+            this.coreService.caculateWPM(this.wordCount, this.timerService.minute() * 60 + this.timerService.second());
+        }
+        this.validateCharacter(event.key);
         this.step++;
+        this.coreService.caculateAccuracy(this.step - this.errorCount, this.step);
+
     }
 }
