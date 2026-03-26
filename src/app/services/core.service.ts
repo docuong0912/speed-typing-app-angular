9b8f9bafd6371b189c9c783f47dbda8f577d0249
docuong0912 <ndcuong912@gmail.com>
2026-03-27 02:24:29 +0700

    add accuracy check

--- src/app/services/core.service.ts
+++ src/app/services/core.service.ts
@@ -4,8 +4,10 @@ import { Injectable, signal } from "@angular/core";
 export class CoreService {
     private _wpm = signal(0);
     private _accuracy = signal(0);
+    private _isAccurate = signal(true);
     readonly wpm = this._wpm.asReadonly();
     readonly accuracy = this._accuracy.asReadonly();
+    readonly isAccurate = this._isAccurate.asReadonly();
 
     caculateWPM(charCount: number, timeInSeconds: number): void {
         const minutes = timeInSeconds / 60;
@@ -15,6 +17,11 @@ export class CoreService {
     caculateAccuracy(correctChars: number, totalChars: number): void {
         if (totalChars === 0) return;
         const accuracy = (correctChars / totalChars) * 100;
+        if (this._accuracy() <= accuracy) {
+            this._isAccurate.set(true);
+        } else {
+            this._isAccurate.set(false);
+        }
         this._accuracy.set(parseFloat(accuracy.toFixed(2)));
     }
 }
