3d5396e91df7eeaccd6b2cab98b19dc901287bfa
docuong0912 <ndcuong912@gmail.com>
2026-03-28 12:34:24 +0700

    handle count down

--- src/app/services/timer.service.ts
+++ src/app/services/timer.service.ts
@@ -7,16 +7,55 @@ export class TimerService {
     private _minute = signal(0);
     readonly second = this._second.asReadonly();
     readonly minute = this._minute.asReadonly();
-    startTimer(): void {
+    initiateTimer(countdown: boolean): void {
         this.stopTimer();
-        this._second.update(v => v + 1);
-        this.timerId = setInterval(() => {
-            this._second.update((v) => v + 1);
-            if (this._second() === 60) {
-                this._second.set(0);
-                this._minute.update((v) => v + 1);
+        if (countdown) {
+            this._second.set(0);
+            this._minute.set(1);
+        }
+        else {
+            this._second.set(0);
+            this._minute.set(0);
+        }
+    }
+
+    startTimer(countdown: boolean): void {
+        this.stopTimer();
+        if (countdown) {
+            this.timerId = setInterval(() => this.handleCountdown(), 1000);
+        }
+        else {
+            this._second.update(v => v + 1);
+            this.timerId = setInterval(() => this.handleCountUp(), 1000);
+        }
+
+    }
+
+    private handleCountUp(): void {
+        this._second.update((v) => {
+            if (v + 1 === 60) {
+                this._minute.update(m => m + 1);
+                return 0;
             }
-        }, 1000);
+            return v + 1;
+        });
+    }
+
+    private handleCountdown(): void {
+        const m = this._minute();
+        const s = this._second();
+
+        if (m === 0 && s === 0) {
+            this.stopTimer();
+            return;
+        }
+
+        if (s === 0) {
+            this._minute.update(v => v - 1);
+            this._second.set(59);
+        } else {
+            this._second.update(v => v - 1);
+        }
     }
 
     stopTimer(): void {
