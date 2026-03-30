4aa6d2184ec5e19403e117897e94c96655a7739c
docuong0912 <ndcuong912@gmail.com>
2026-03-31 01:54:01 +0700

    handle result

--- src/app/services/core.service.ts
+++ src/app/services/core.service.ts
@@ -5,9 +5,13 @@ export class CoreService {
     private _wpm = signal(0);
     private _accuracy = signal(0);
     private _isAccurate = signal(true);
+    private _testEnded = signal(false);
     readonly wpm = this._wpm.asReadonly();
     readonly accuracy = this._accuracy.asReadonly();
     readonly isAccurate = this._isAccurate.asReadonly();
+    readonly testEnded = this._testEnded.asReadonly();
+    correctChars = 0;
+    incorrectChars = 0;
 
     caculateWPM(charCount: number, timeInSeconds: number): void {
         const minutes = timeInSeconds / 60;
@@ -23,5 +27,8 @@ export class CoreService {
             this._isAccurate.set(false);
         }
         this._accuracy.set(parseFloat(accuracy.toFixed(2)));
+    }
+    endTest(): void {
+        this._testEnded.set(true);
     }
 }
